3fd683c50471302d8c6f92fd154eecca16bce35b
mhayakusa <mhayakusa@hi-net.zaq.ne.jp>
2023-06-14 13:09:18 +0900

    test

--- app.py
+++ app.py
@@ -116,5 +116,5 @@ def kanjitest():
     return render_template('kanjitest.html', kdata=kdata,mojinum=moji_num,setumei=setumei)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
